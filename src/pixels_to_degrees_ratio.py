3a033d31c52da345c01c76845b27e268b7e182cd
Ali Albustami <alibustami3@gmail.com>
2023-04-04 23:54:22 +0300

    Revert "Convert_pixels_to_degrees"
    This reverts commit 357bbb2cf49aa8035b64b11baf5d304d17687c2c.

--- src/pixels_to_degrees_ratio.py
+++ src/pixels_to_degrees_ratio.py
@@ -1,9 +1,1 @@
 """This module contains the pixel to degrees ratio calculator."""
-
-def convert_pixels_to_degrees( img, distance_list, wheel_diameter):
-    img_scale = img.shape[1] / 236.2
-    wheel_scale = (wheel_diameter * 3.14) / 360
-    degrees_list = []
-    for distance in distance_list:
-        degrees_list.append(round((distance / img_scale) / wheel_scale))
-    return degrees_list
