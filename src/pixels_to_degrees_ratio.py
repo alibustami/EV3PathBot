507c6a9f8ab77cd8a55d6fe5f52fb50373d9e395
Ali Albustami <alibustami3@gmail.com>
2023-04-05 01:48:31 +0300

    add typing to variables

--- src/pixels_to_degrees_ratio.py
+++ src/pixels_to_degrees_ratio.py
@@ -1,26 +1,31 @@
 """This module contains the pixel to degrees ratio calculator."""
+from typing import List
+
+import numpy as np
 
 
-def convert_pixels_to_degrees(img, distance_list, wheel_diameter):
+def convert_pixels_to_degrees(
+    img: np.array, distance_list: List[float], wheel_diameter: float
+) -> List[int]:
     """Convert pixels to degrees.
 
     Parameters
     ----------
     img : np.array
         table image
-    distance_list : list
+    distance_list : List[float]
         the input distances
     wheel_diameter : float
         wheel diameter
 
     Returns
     -------
-    list
+    List[int]
         the degrees the robot should move
     """
-    img_scale = img.shape[1] / 236.2
-    wheel_scale = (wheel_diameter * 3.14) / 360
-    degrees_list = []
+    img_scale: float = img.shape[1] / 236.2
+    wheel_scale: float = (wheel_diameter * 3.14) / 360
+    degrees_list: list = []
     for distance in distance_list:
         degrees_list.append(round((distance / img_scale) / wheel_scale))
     return degrees_list
