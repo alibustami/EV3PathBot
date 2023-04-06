6c83451d21af7a0685341290b073b1478e6adefb
waleed <waleedabublam@gmail.com>
2023-04-06 22:34:53 +0300

    remove unnecessary imports

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,6 +1,5 @@
 """This module contains tests for image_reader.py module."""
 import unittest
-from unittest.mock import patch
 
 import cv2 as cv
 import numpy as np
@@ -9,10 +8,10 @@ from src import image_reader
 
 
 class TestImage(unittest.TestCase):
-    """This class contains tests for image reading"""
+    """This class contains tests for image reading."""
 
-
-def test_a(self):
-    path = r"C:\Users\walee\Desktop\final\cargo1.jpg"
-    img: np.ndarray = cv.imread(path)
-    self.assertTrue(np.array_equal(img, image_reader.image_validation(path)))
+    def test_image_reader(self):
+        """This function contains tests for image reading."""
+        path = r"C:\Users\walee\Desktop\final\cargo1.jpg"
+        img: np.ndarray = cv.imread(path)
+        self.assertTrue(np.array_equal(img, image_reader.image_validation(path)))
