9f9b6455850fd3ffa6c424810cfa9df7f1da4ad8
Hazem Albtoush <hazem.albtoush@gmail.com>
2023-04-06 21:30:35 +0300

    change test arguments

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,6 +1,6 @@
 """This module is for testing the pixels to degrees converter."""
 import unittest
-from typing import List
+from typing import List, Tuple
 
 import numpy as np
 
@@ -12,11 +12,11 @@ class TestConvertPixelsToDegrees(unittest.TestCase):
 
     def test_convert_pixels_to_degrees(self):
         """Test the converter."""
-        img = np.zeros((100, 236), dtype=np.uint8)
+        img_dims: Tuple[int] = (0, 236, 0)
         distance_list: List[float] = [100, 200, 300]
         wheel_diameter: float = 10
         expected_degrees_list: List[float] = [1147, 2295, 3442]
         result_degrees_list: List[float] = convert_pixels_to_degrees(
-            img, distance_list, wheel_diameter
+            img_dims, distance_list, wheel_diameter
         )
         self.assertEqual(result_degrees_list, expected_degrees_list)
