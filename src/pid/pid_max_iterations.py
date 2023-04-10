c99a21389aa109eada312f4ba7c58f2e0562261b
Ali Albustami <alibustami3@gmail.com>
2023-04-10 03:18:43 +0300

    refactor pid_max_iterations and it's test cases

--- src/pid/pid_max_iterations.py
+++ src/pid/pid_max_iterations.py
@@ -1,17 +1,20 @@
 """This module contains the PID controller using max iterations as the termination condition."""
-from typing import Tuple, Union
+from typing import Optional, Tuple, Union
+
+from src.pid.core_pid import pid_control
+from src.utils import extract_pid_constants
 
 
 def pid_max_iterations(
     process_variable: Union[int, float],
     set_point: Union[int, float],
-    constants: Tuple[float],
-    appecpted_error: Union[int, float],
-    last_error: int,
-    sum_of_errors: int,
-    num_iterations: int,
-    check_iterations: int = 0,
-) -> Tuple[Union[int, float]]:
+    sensor: str,
+    iterations: int,
+    sum_of_errors: Union[int, float],
+    last_error: Union[int, float],
+    constants: Optional[Tuple[float]] = None,
+    accepted_error: Optional[Union[int, float]] = None,
+) -> Union[int, float]:
     """Calculate the PID control.
 
     Parameters
@@ -20,67 +23,37 @@ def pid_max_iterations(
         The process variable.
     set_point : Union[int, float]
         The set point.
+    sensor : str
+        The sensor name.
+    iterations : int
+        The number of iterations.
     constants : Tuple[float]
         The constants for the PID controller (kp, ki, kd).
-    appecpted_error : Union[int, float]
+    accepted_error : Union[int, float]
         The accepted error.
-    last_error : int
-        The last error.
-    sum_of_errors : int
-        The sum of errors.
-    num_iterations : int
-        The number of iterations to perform.
-    check_iterations : int
+
     Returns
     -------
-    Tuple[Union[int, float]]
-        The PID control, sum of errors and last error, check iterations.
+    Union[int, float]
+        The PID control value.
     """
-    kp, ki, kd = constants
-    pid = 0
+    pid_configs = extract_pid_constants(sensor)
 
-    for i in range(num_iterations):
-        error: Union[int, float] = set_point - process_variable
-        sum_of_errors += error
+    kp = constants[0] if constants else pid_configs.kp
+    ki = constants[1] if constants else pid_configs.ki
+    kd = constants[2] if constants else pid_configs.kd
+    accepted_error = accepted_error if accepted_error else pid_configs.accepted_error
 
-        if abs(error) < appecpted_error:
-            pid: int = 0
-        else:
-            # P-Term Calculation
-            p_term: Union[int, float] = kp * error
+    iterations_counter: int = 0
+    for _ in range(iterations):
+        pid, sum_of_errors, last_error = pid_control(
+            process_variable,
+            set_point,
+            (kp, ki, kd),
+            accepted_error,
+            last_error,
+            sum_of_errors,
+        )
+        iterations_counter += 1
 
-            # I-Term Calculation
-            i_term: Union[int, float] = ki * sum_of_errors
-
-            # D-Term Calculation
-            d_term: Union[int, float] = kd * last_error
-
-            last_error = error
-
-            pid: Union[int, float] = p_term + i_term + d_term
-
-        check_iterations += 1
-
-    return pid, sum_of_errors, last_error, check_iterations
-
-
-process_variable: int = 20
-set_point: int = 0
-constants: tuple = (1, 1, 1)
-appecpted_error: int = 0
-last_error: int = 0
-sum_of_errors: int = 0
-num_iterations: int = 5
-check_iterations: int = 0
-
-pid_value, sum_of_errors, last_error, check_iterations = pid_max_iterations(
-    process_variable,
-    set_point,
-    constants,
-    appecpted_error,
-    last_error,
-    sum_of_errors,
-    num_iterations,
-    check_iterations,
-)
-print(pid_value)
+    return pid
