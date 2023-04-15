5f60009ec7807e56fbcb6b395bc480cf0a227aaa
Ali Albustami <alibustami3@gmail.com>
2023-04-15 03:42:32 +0300

    add motors extraction

---
+++ tests/test_motors_extraction.py
@@ -0,0 +1,33 @@
+"""Test cases for motors extraction."""
+import unittest
+from unittest.mock import patch
+
+from src.motors_extraction import motors_extraction
+
+
+class TestMotorsExtraction(unittest.TestCase):
+    """Test cases for motors extraction."""
+
+    @patch(
+        "src.motors_extraction.get_config",
+        side_effect=lambda key: {
+            "motor_a": "large",
+            "motor_b": "medium",
+            "motor_c": "large",
+            "motor_d": "medium",
+        }
+        if key == "robot_motors"
+        else None,
+    )
+    def test_motors_extraction(self, mock_get_config):
+        """test_motors_extraction."""
+        expected_large_motors_list = ["a", "c"]
+        expected_medium_motors_list = ["b", "d"]
+        self.assertEqual(
+            motors_extraction(), (expected_large_motors_list, expected_medium_motors_list)
+        )
+
+    def test_motors_extraction_exception(self):
+        """test_motors_extraction_exception."""
+        with self.assertRaises(Exception):
+            motors_extraction()
