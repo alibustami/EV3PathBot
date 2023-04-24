4f2962ca27457c5997cf5ab01869dd386645919f
Ali Albustami <alibustami3@gmail.com>
2023-04-24 19:25:45 +0300

    remove log file append option

--- src/main.py
+++ src/main.py
@@ -1,24 +1,15 @@
 """This module contains the main code for EV3PathBot."""
 import logging
 
-from src.configs import get_config
 from src.GUIs.main_screen import run
 from src.path_creation import create_path
 from src.Writer.code_writer import CodeEditor
-
-log_file_append = get_config("log_file_append")
-if not log_file_append:
-    log_file_append = False
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(levelname)s: on file %(filename)s, on line %(lineno)d: %(message)s",
     filename="logs.log",
-    filemode="w" if not log_file_append else "a",
-)
-
-logging.info(
-    f"logging file mode is {'append' if log_file_append else 'write'}, you can change it in the config file"
+    filemode="w",
 )
 
 (
